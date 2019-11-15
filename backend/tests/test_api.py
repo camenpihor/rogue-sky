1ae2d736a1d5707215b5bda12dfa309042969c8e
Camen <camenpihor@gmail.com>
2019-11-14 22:24:14 -0800

    Fix api test; add star forecast api test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -23,11 +23,36 @@ def test_weather_forecast(
         ),
         json=darksky_json_response,
     )
-    response = backend_api_client.get(f"/weather/{latitude},{longitude}")
+    response = backend_api_client.get(f"api/weather/{latitude},{longitude}")
     assert response.status_code == 200
 
     actual = json.loads(response.data)
     assert len(actual) == 8
     assert set(actual["0"]["weather_json"].keys()) == set(
         darksky.DAILY_WEATHER_MAPPING.keys()
+    )
+
+
+def test_weather_forecast(
+    requests_mock, backend_api_client, test_database, darksky_json_response
+):
+    latitude = 47.6062
+    longitude = 122.3321
+    api_key = backend_api_client.application.config["DARKSKY_API_KEY"]
+
+    postgres_utilities.create_weather_table(pg_url=test_database.pg_url)
+    postgres_utilities.create_star_table(pg_url=test_database.pg_url)
+    requests_mock.get(
+        darksky.DARKSKY_URL.format(
+            api_key=api_key, latitude=latitude, longitude=longitude
+        ),
+        json=darksky_json_response,
+    )
+    response = backend_api_client.get(f"api/stars/{latitude},{longitude}")
+    assert response.status_code == 200
+
+    actual = json.loads(response.data)
+    assert len(actual) == 8
+    assert set(actual["0"].keys()) == set(
+        ["latitude", "longitude", "weather_date_utc", "queried_date_utc", "prediction"]
     )
